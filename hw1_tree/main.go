7aaa5d4dd3559acdb5fcc59490ea96bc47b41db4
Georgich88 <georgeisaev@gmail.com>
2019-06-19 07:16:51 +0700

    Create a project
    Create a project.
    Deliver files from Coursera.

---
+++ hw1_tree/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+)
+
+func main() {
+	out := os.Stdout
+	if !(len(os.Args) == 2 || len(os.Args) == 3) {
+		panic("usage go run main.go . [-f]")
+	}
+	path := os.Args[1]
+	printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
+	err := dirTree(out, path, printFiles)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func dirTree(out *os.File, path string, printFiles bool) error {
+	return nil // ToDo: develop method
+}
