235a3aba427228bc9541590c29f6d03b72a1ddd2
Georgich88 <georgeisaev@gmail.com>
2019-06-19 08:51:48 +0700

    Print files with pipline bugs
    Need to work with pipeline of the directory level if it is not the last file in a list.

--- hw1_tree/main.go
+++ hw1_tree/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"sort"
+	"strings"
 )
 
 func main() {
@@ -18,5 +21,54 @@ func main() {
 }
 
 func dirTree(out *os.File, path string, printFiles bool) error {
-	return nil // ToDo: develop method
+
+	return dirTreePrint(out, path, printFiles, 0, false)
+
+}
+
+func dirTreePrint(out *os.File, path string, printFiles bool, level int, continuePrint bool) error {
+
+	directory, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+
+	files, err := directory.Readdir(-1)
+	directory.Close()
+	if err != nil {
+		return err
+	}
+
+	sort.Slice(files, func(i, j int) bool { return files[i].Name() < files[j].Name() })
+
+	size := len(files)
+	for i, file := range files {
+
+		if file.IsDir() || printFiles {
+			if level > 0 {
+				fmt.Fprint(out, strings.Repeat("\t", level-1))
+				if continuePrint {
+					fmt.Fprint(out, "│")
+				}
+				fmt.Fprint(out, "\t")
+			}
+			if i == size-1 {
+				fmt.Fprint(out, "└───")
+			} else {
+				fmt.Fprint(out, "├───")
+			}
+			fmt.Fprintln(out, file.Name())
+
+		}
+		if file.IsDir() {
+			dirPath := path + string(os.PathSeparator) + file.Name()
+			err := dirTreePrint(out, dirPath, printFiles, level+1, i != size-1)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+
 }
